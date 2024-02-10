a6e2a81d0d82ea24f9d256d6e81d0aeba47d3361
byManuel18 <antoniomanu533@gmail.com>
2024-02-10 19:32:19 +0100

    Add PrintService

---
+++ src/app/shared/services/print.service.ts
@@ -0,0 +1,39 @@
+import { HostListener, Injectable, computed, signal } from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrintService {
+
+
+  private $beforePrint!: Subscription;
+  private $afterPrint!: Subscription;
+
+  private readonly _isPrint = signal<boolean>(false);
+
+  public isPrint = computed(()=>this._isPrint());
+
+  constructor () {
+    this.configEvents();
+  }
+
+  configEvents(){
+
+    this.$afterPrint = fromEvent(window,'afterprint').subscribe((_)=>{
+      this._isPrint.set(false);
+    });
+
+    this.$beforePrint = fromEvent(window,'beforeprint').subscribe((_)=>{
+      this._isPrint.set(true);
+    });
+
+  }
+
+  stopListenners() {
+    this._isPrint.set(false);
+    this.$afterPrint?.unsubscribe();
+    this.$beforePrint?.unsubscribe();
+  }
+
+}
