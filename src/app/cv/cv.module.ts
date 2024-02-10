f9e20b24a659d9e9b4f08ee5828bc127cbc57a5a
byManuel18 <antoniomanu533@gmail.com>
2024-02-10 22:00:06 +0100

    add layout footer interface cv

--- src/app/cv/cv.module.ts
+++ src/app/cv/cv.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CvRoutingModule } from './cv-routing.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { LayoutPageComponent } from './layout/layout-page/layout-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     CvRoutingModule
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CvModule { }
--- src/app/cv/layout/layout-page/layout-page.component.ts
+++ src/app/cv/layout/layout-page/layout-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
+import 'ninja-keys';
+
 @Component({
-  selector: 'app-layout-page',
-  standalone: true,
-  imports: [],
+  selector: 'cv-layout-page',
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.css'
 })
