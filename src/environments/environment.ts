f0ebf0aa319c487c0f4fc9730d833ed6b7494acb
byManuel18 <antoniomanu533@gmail.com>
2024-07-07 10:57:15 +0200

    remove firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    firebaseConfig : {
-        apiKey: "AIzaSyCm-Ae7iwT1e7FKfiRxL4DTg-fzOfgXojY",
-        authDomain: "cv-app-22942.firebaseapp.com",
-        projectId: "cv-app-22942",
-        storageBucket: "cv-app-22942.appspot.com",
-        messagingSenderId: "719996750667",
-        appId: "1:719996750667:web:f8dd3ab3f82455ad85f6ce"
-      }
+  firebaseConfig : {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
 };
